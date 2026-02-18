7c19d4310a9d969949c59f3829a600f1aeb2bcba
funky_galileo <kishoreanand275@gmail.com>
2026-02-18 13:26:44 +0530

    new method2

---
+++ src/test/java/com/omrbranch/runner/TestRunner2.java
@@ -0,0 +1,22 @@
+package com.omrbranch.runner;
+
+public class TestRunner2 {
+	
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void method4() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void method5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
